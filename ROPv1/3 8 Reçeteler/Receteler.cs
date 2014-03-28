508f3dac863f2b3673bea4cf5967ca6376a4c690
busraozgode <busraozgode@gmail.com>
2014-03-28 14:06:24 +0200

    reçeteler

---
+++ ROPv1/3 8 Reçeteler/Receteler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROPv1
+{
+    public partial class Receteler : UserControl
+    {
+        public Receteler()
+        {
+            InitializeComponent();
+        }
+
+        private void keyboardcontrol1_UserKeyPressed(object sender, KeyboardClassLibrary.KeyboardEventArgs e)
+        {
+            SendKeys.Send(e.KeyboardKeyPressed);
+        }
+        private void Receteler_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
